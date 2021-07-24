801f76451a3d3028c8c4191d282503aea1536717
aybek can kaya <aybek.kaya@rapsodo.com>
2021-07-24 03:55:32 +0300

    pck

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "UINib+Loading",
+    platforms: [.macOS(.v10_10), .iOS(.v13), .watchOS(.v2), .tvOS(.v9) ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
